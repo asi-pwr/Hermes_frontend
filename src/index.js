3a082b45fad30176318fca0f38333b48d87c71ab
KarolKalbarczyk <karol539@wp.pl>
2019-12-23 20:16:36 +0100

    feat: added buttons displayed below each other

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Button from '@material-ui/core/Button'
+
+const App = () => {
+    return <div>
+      <div><Button variant="contained" color="primary" onClick={addTemplate}>addTemplate</Button></div>
+      <div><Button variant="contained" color="primary" onClick={modifyTemplate}>modifyTemplate</Button></div>
+      <div><Button variant="contained" color="primary" onClick={deleteTemplate}>deleteTemplate</Button></div>
+      <div><Button variant="contained" color="primary" onClick={sendMail}>sendMail</Button></div>
+      </div>
+}
+
+
+const addTemplate = () => {}
+const modifyTemplate = () => {}
+const deleteTemplate = () => {}
+const sendMail = () => {}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
