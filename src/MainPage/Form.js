a38c9ed6e70581ba09fedd1caf2d035a4771b0fa
KarolKalbarczyk <karol539@wp.pl>
2020-01-02 21:06:14 +0100

    style: project
    fix issues pointed out in pull request review

---
+++ src/MainPage/Form.js
@@ -0,0 +1,27 @@
+import React,{ useState } from 'react';
+import '../center.css';
+import Buttons from './Buttons.js'
+
+const Form = () => {
+    const [subject,setSubject] = useState("");
+    const [content,setContent] = useState("");
+    return (
+      <div className="main-page-form">
+        <div>
+          <form>
+            <label className = "main-page-form__centralised-element">
+              <div>Subject:</div>
+              <input  type="text"  value={subject} onChange={(e) => setSubject(e.target.value)} />
+            </label>
+            <label className = "main-page-form__centralised-element">
+              Content:
+              <textarea className =  "main-page-form__message-content-textarea" value={content} onChange={(e) => setContent(e.target.value)}/>
+            </label>
+          </form>
+        </div>
+        <div><Buttons subject={subject} content = {content} ></Buttons></div>
+      </div>
+    );
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './center.css'
-//import App from './App';
-import MainPage from './MainPage';
+import MainPage from './MainPage/MainPage';
 import * as serviceWorker from './serviceWorker';
-import Button from '@material-ui/core/Button'
-import { string } from 'prop-types';
-import zIndex from '@material-ui/core/styles/zIndex';
-import { Grid } from '@material-ui/core';
 
 const App = () => {
-    return <div >
-      <MainPage/>
+    return (
+      <div >
+        <MainPage/>
       </div>
+    );
 }
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
